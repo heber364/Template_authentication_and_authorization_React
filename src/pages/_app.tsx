d68c4fcb5fa5c1c97e6055d05c758307be3a03a0
Héber Lima Silva <heberlimasilva@gmail.com>
2022-12-03 08:40:35 -0300

    Contexto de autenticação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,15 @@ import React from "react";
 import { ChakraProvider, theme } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
+import { AuthProvider } from "../contexts/AuthContext";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <AuthProvider>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </AuthProvider>
   );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,42 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { FormEvent, useContext, useState } from "react";
+
+import { Button, Flex, Input, Text } from "@chakra-ui/react";
 import type { NextPage } from "next";
 
+import { AuthContext } from "../contexts/AuthContext";
+
 const Home: NextPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { isAuthenticated, signIn } = useContext(AuthContext);
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    const data = {
+      email,
+      password,
+    };
+
+    console.log(data);
+  }
   return (
     <Flex h="100vh" justifyContent="center" alignItems="center">
-      <Text>Este é o template de React para CT Junior, criado em 2022</Text>
+      <Flex gap="2" direction="column" as="form" onSubmit={() => handleSubmit}>
+        <Input
+          placeholder="E-mail"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button type="submit">Entrar</Button>
+      </Flex>
     </Flex>
   );
 };
